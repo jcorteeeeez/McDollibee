3bdd6d5354feba012a5fc5ebfe5cb3de28c6cdc8
jcorteeeeez <CORTEZ.JOHANNESCHRISTOPHER@UE.EDU.PH>
2021-11-21 22:51:57 +0800

    Add project files.

---
+++ McDollibee/Form2.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace McDollibee
+{
+    public partial class Form2 : Form
+    {
+        Form1 form1;
+
+        public Form2()
+        {
+        }
+
+        public Form2(Form1 frm1)
+        {
+            InitializeComponent();
+            this.form1 = frm1;
+        }
+    }
+}
